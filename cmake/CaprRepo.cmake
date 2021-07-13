de1590f2d0cadb255802e348179480b2a68ec5a8
ElandaOfficial <elendaofficial@gmail.com>
2021-07-13 13:43:01 +0200

    Creating base of capr and adding inbuilt package cson

--- cmake/CaprRepo.cmake
+++ cmake/CaprRepo.cmake
@@ -1,2 +1,13 @@
 ########################################################################################################################
+function(parse_repository_index index_file)
+    file(READ ${CAPR_INDEX_FILE} index_content)
+    string(JSON repository_array_len ERROR_VARIABLE json_error LENGTH ${index_content} repositories)
 
+    if (json_error)
+        message(FATAL_ERROR "Couldn't read CAPR repository index: ${json_error}")
+    endif()
+
+    foreach(i RANGE )
+        string(JSON repository_array ERROR_VARIABLE json_error GET ${index_content} repositories)
+    endforeach()
+endfunction()
---
+++ cmake/inbuilt/cson/Cson.cmake
@@ -0,0 +1,161 @@
+########################################################################################################################
+set(CSON_DEFAULT_SEVERITY "FATAL_ERROR" CACHE STRING "The default log level for any CSON function call")
+
+########################################################################################################################
+macro(cson_internal_log_message)
+    if (NOT "${parse_error}" STREQUAL "NOTFOUND")
+        if (NOT DEFINED ARG_SEVERITY OR "SEVERITY" IN_LIST ARG_KEYWORDS_MISSING_VALUES)
+            set(ARG_SEVERITY "${CSON_DEFAULT_SEVERITY}")
+        endif()
+
+        string(TOLOWER ${ARG_SEVERITY} log_severity)
+
+        if ("${log_severity}" STREQUAL "fatal_error")
+            message(FATAL_ERROR "${parse_error}")
+        elseif("${log_severity}" STREQUAL "send_error")
+            message(SEND_ERROR "${parse_error}")
+        elseif("${log_severity}" STREQUAL "warning")
+            message(WARNING "${parse_error}")
+        elseif("${log_severity}" STREQUAL "status")
+            message(STATUS "${parse_error}")
+        elseif("${log_severity}" STREQUAL "verbose")
+            message(VERBOSE "${parse_error}")
+        elseif("${log_severity}" STREQUAL "debug")
+            message(DEBUG "${parse_error}")
+        elseif("${log_severity}" STREQUAL "trace")
+            message(TRACE "${parse_error}")
+        else()
+            message(NOTICE "${parse_error}")
+        endif()
+    endif()
+endmacro()
+
+macro(cson_internal_json output)
+    string(JSON ${output} ERROR_VARIABLE parse_error ${ARGN})
+    cson_internal_log_message()
+endmacro()
+
+macro(cson_internal_process_member member_prefix member type)
+    if ("${member_prefix}" STREQUAL "")
+        set(${member_prefix}_opt_dot "")
+    else()
+        set(${member_prefix}_opt_dot .)
+    endif()
+
+    if ("${type}" STREQUAL "ARRAY" OR "${type}" STREQUAL "OBJECT")
+        cson_internal_json(
+            ${member_prefix}_member_length
+            LENGTH ${member})
+
+        math(EXPR ${member_prefix}_last_index "${${member_prefix}_member_length} - 1")
+
+        if ("${type}" STREQUAL "ARRAY")
+        
+            if (${${member_prefix}_member_length} GREATER 0)
+                foreach(${member_prefix}_i RANGE ${${member_prefix}_last_index})
+                    cson_internal_json(
+                        ${member_prefix}_${${member_prefix}_i}_type
+                        TYPE ${member} ${${member_prefix}_i})
+
+                    cson_internal_json(
+                        ${member_prefix}_${${member_prefix}_i}_value
+                        GET ${member} ${${member_prefix}_i})
+
+                    if ("${${member_prefix}_${${member_prefix}_i}_type}" STREQUAL "ARRAY" OR "${${member_prefix}_${${member_prefix}_i}_type}" STREQUAL "OBJECT")
+                        cson_internal_process_member(
+                            ${member_prefix}+${${member_prefix}_i}
+                            "${${member_prefix}_${${member_prefix}_i}_value}"
+                            "${${member_prefix}_${${member_prefix}_i}_type}")
+                        
+                        list(APPEND ${member_prefix}_result ${prefix}${${member_prefix}_opt_dot}${member_prefix}+${${member_prefix}_i})
+                    else()
+                        list(APPEND ${member_prefix}_result ${${member_prefix}_${${member_prefix}_i}_value})
+                        set(${prefix}${${member_prefix}_opt_dot}${member_prefix}+${${member_prefix}_i} ${${member_prefix}_${${member_prefix}_i}_value} PARENT_SCOPE)
+                    endif()
+                endforeach()
+            endif()
+
+            if (NOT "${member_prefix}" STREQUAL "")
+                list(APPEND CSON_LIST_ARRAY_${prefix} ${prefix}${${member_prefix}_opt_dot}${member_prefix})
+            endif()
+        else()
+            if (${${member_prefix}_member_length} GREATER 0)
+                foreach(${member_prefix}_i RANGE ${${member_prefix}_last_index})
+                    cson_internal_json(
+                        ${member_prefix}_${${member_prefix}_i}_name
+                        MEMBER ${member} ${${member_prefix}_i})
+
+                    cson_internal_json(
+                        ${member_prefix}_${${member_prefix}_i}_type
+                        TYPE ${member} ${${member_prefix}_${${member_prefix}_i}_name})
+
+                    cson_internal_json(
+                        ${member_prefix}_${${member_prefix}_i}_value
+                        GET ${member} ${${member_prefix}_${${member_prefix}_i}_name})
+
+                    cson_internal_process_member(
+                        ${member_prefix}${${member_prefix}_opt_dot}${${member_prefix}_${${member_prefix}_i}_name}
+                        "${${member_prefix}_${${member_prefix}_i}_value}"
+                        "${${member_prefix}_${${member_prefix}_i}_type}")
+
+                    list(APPEND ${member_prefix}_result ${prefix}${${member_prefix}_opt_dot}${member_prefix}.${${member_prefix}_${${member_prefix}_i}_name})
+                endforeach()
+            endif()
+
+            if (NOT "${member_prefix}" STREQUAL "")
+                list(APPEND CSON_LIST_OBJECT_${prefix} ${prefix}${${member_prefix}_opt_dot}${member_prefix})
+            endif()
+        endif()
+    else()
+        set(${member_prefix}_result ${member})
+
+        if ("${type}" STREQUAL "NUMBER")
+            list(APPEND CSON_LIST_NUMBER_${prefix} ${member_prefix})
+        elseif("${type}" STREQUAL "STRING")
+            list(APPEND CSON_LIST_STRING_${prefix} ${member_prefix})
+        elseif("${type}" STREQUAL "BOOLEAN")
+            list(APPEND CSON_LIST_BOOL_${prefix} ${member_prefix})
+        else()
+            list(APPEND CSON_LIST_NULL_${prefix} ${member_prefix})
+        endif()
+    endif()
+
+    set(${prefix}${${member_prefix}_opt_dot}${member_prefix} ${${member_prefix}_result} PARENT_SCOPE)
+endmacro()
+
+function(cson_parse_json prefix json_text)
+    cmake_parse_arguments(ARG "" "SEVERITY" "" ${ARGN})
+
+    string(TOLOWER "${prefix}" lower_prefix)
+
+    if ("${lower_prefix}" MATCHES "^cson")
+        set(parse_error "A prefix can't start with ${prefix}")
+        cson_internal_log_message()
+    endif()
+
+    cson_internal_json(
+        root_type
+        TYPE "${json_text}")
+
+    cson_internal_process_member("" "${json_text}" "${root_type}")
+
+    set(CSON_LIST_NUMBER_${prefix} ${CSON_LIST_NUMBER_${prefix}} PARENT_SCOPE)
+    set(CSON_LIST_STRING_${prefix} ${CSON_LIST_STRING_${prefix}} PARENT_SCOPE)
+    set(CSON_LIST_BOOL_${prefix}   ${CSON_LIST_BOOL_${prefix}}   PARENT_SCOPE)
+    set(CSON_LIST_NULL_${prefix}   ${CSON_LIST_NULL_${prefix}}   PARENT_SCOPE)
+    set(CSON_LIST_ARRAY_${prefix}  ${CSON_LIST_ARRAY_${prefix}}  PARENT_SCOPE)
+    set(CSON_LIST_OBJECT_${prefix} ${CSON_LIST_OBJECT_${prefix}} PARENT_SCOPE)
+endfunction()
+
+function(cson_include prefix json_file)
+    cmake_parse_arguments(ARG "" "SEVERITY" "" ${ARGN})
+
+    if (NOT EXISTS json_file)
+        set(parse_error "Could not find file ${json_file}")
+        cson_internal_log_message()
+        return()
+    endif()
+
+    file(READ ${json_file} json_file_output)
+    cson_parse_json(${prefix} "${json_file_output}" SEVERITY ${ARG_SEVERITY})
+endfunction()
